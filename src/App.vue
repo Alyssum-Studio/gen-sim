7fc36fd30f2cb70e969d62dff0e068d40180d3b4
Kevin Ju <kevinju0827@gmail.com>
2023-01-04 20:18:18 +0800

    Create page skeleton.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template lang="pug">
 v-app
+  v-app-bar(title="GenSim")
+    v-btn(icon)
+      v-icon mdi-help
+    v-btn(icon)
+      v-icon mdi-earth
   v-main
-    HelloWorld
+    router-view
 </template>
 
 <script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
 </script>
--- src/layouts/default/Default.vue
+++
@@ -1,8 +0,0 @@
-<template lang="pug">
-v-app
-  default-view
-</template>
-
-<script lang="ts" setup>
-  import DefaultView from './View.vue'
-</script>
