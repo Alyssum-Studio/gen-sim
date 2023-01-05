c3b52739d412b250709d9d3df9c2ca71906e287e
Kevin Ju <kevinju0827@gmail.com>
2023-01-06 00:10:14 +0800

    Update app bar icons.

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
 v-app
   v-app-bar(title="GenSim")
     v-btn(icon)
-      v-icon mdi-help
-    v-btn(icon)
       v-icon mdi-earth
+    v-btn(icon)
+      v-icon mdi-github
   v-main
     router-view
 </template>
