261e0f782426ac0ff6fa0f07c3c9f942bdf0c875
Kevin Ju <kevinju0827@gmail.com>
2023-03-04 17:14:32 +0800

    Add GitHub link.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ v-app
   v-app-bar(title="GenSim")
     v-btn(icon)
       v-icon mdi-earth
-    v-btn(icon)
+    v-btn(icon href="https://github.com/Alyssum-Studio/gen-sim")
       v-icon mdi-github
   v-main
     router-view
